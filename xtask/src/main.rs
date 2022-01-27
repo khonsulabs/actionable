a95b507bdd8fa7046806f3f8c0f9b1989177b72b
Jonathan Johnson <jon@khonsulabs.com>
2022-01-27 09:02:59 -0800

    Updating xtask to include all commands

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,30 +1,40 @@
-use khonsu_tools::universal::{
-    anyhow,
-    code_coverage::{self, CodeCoverage},
-};
-use structopt::StructOpt;
-
-#[derive(StructOpt, Debug)]
-pub enum Commands {
-    GenerateCodeCoverageReport {
-        #[structopt(long = "install-dependencies")]
-        install_dependencies: bool,
+use khonsu_tools::{
+    universal::{
+        anyhow,
+        clap::Parser,
+        code_coverage::{self},
+        DefaultConfig,
     },
-}
+    Commands,
+};
 
 fn main() -> anyhow::Result<()> {
-    let command = Commands::from_args();
-    match command {
-        Commands::GenerateCodeCoverageReport {
-            install_dependencies,
-        } => CodeCoverage::<CoverageConfig>::execute(install_dependencies),
+    Commands::parse().execute::<Config>()
+}
+
+struct Config;
+
+impl khonsu_tools::Config for Config {
+    type Publish = Self;
+    type Universal = Self;
+}
+
+impl khonsu_tools::universal::Config for Config {
+    type Audit = DefaultConfig;
+    type CodeCoverage = Self;
+}
+
+impl code_coverage::Config for Config {
+    fn ignore_paths() -> Vec<String> {
+        vec![String::from("actionable/examples/*")]
     }
 }
 
-struct CoverageConfig;
-
-impl code_coverage::Config for CoverageConfig {
-    fn ignore_paths() -> Vec<String> {
-        vec![String::from("actionable/examples/*")]
+impl khonsu_tools::publish::Config for Config {
+    fn paths() -> Vec<String> {
+        vec![
+            String::from("actionable-macros"),
+            String::from("actionable"),
+        ]
     }
 }
