9701f2ff59a59446971f76dcbdce013495409d18
kl-botsu <botsu@khonsulabs.com>
2022-03-10 17:48:12 +0000

    Deploying to gh-pages from @ khonsulabs/actionable@5aa3a1f4f4afda76ee3c8c45e9f87c44910d70c2 🚀

--- main/implementors/core/hash/trait.Hash.js
+++ main/implementors/core/hash/trait.Hash.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["actionable"] = [{"text":"impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.58.1/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"actionable/enum.Identifier.html\" title=\"enum actionable::Identifier\">Identifier</a>&lt;'a&gt;","synthetic":false,"types":["actionable::statement::Identifier"]}];
+implementors["actionable"] = [{"text":"impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.59.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"actionable/enum.Identifier.html\" title=\"enum actionable::Identifier\">Identifier</a>&lt;'a&gt;","synthetic":false,"types":["actionable::statement::Identifier"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
